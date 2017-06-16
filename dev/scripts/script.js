196f29f26ffd426a569242bfa456cab04727f9f2
Vincent Chevalier <info@vincentpchevalier.com>
2017-06-16 14:19:04 -0400

    scss styles for gallery, blog, contact

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,12 +1,1 @@
-var elem = document.querySelector('.grid');
-var iso = new Isotope( elem, {
-  // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows'
-});
 
-// element argument can be a selector string
-//   for an individual element
-var iso = new Isotope( '.grid', {
-  // options
-});
