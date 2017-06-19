8b8fe4f969a5fb77ce42caa55dbe9bef95bbbc7f
Vincent Chevalier <info@vincentpchevalier.com>
2017-06-19 19:36:48 -0400

    styles and uploading final assets

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -11,7 +11,21 @@ mySite.events = () => {
 		$('.menu-bar').toggleClass('overlay');
 
 		//toggle the show class on the ul inside of menu
-		$('.menu-bar ul').toggleClass('show');
+		$('.main-menu').toggleClass('show');
+	});
+
+	//smooth scroll
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	    var target = $(this.hash);
+	    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	    if (target.length) {
+	      $('html, body').animate({
+	        scrollTop: target.offset().top
+	      }, 1000);
+	      return false;
+	    }
+	  }
 	});
 }
 
