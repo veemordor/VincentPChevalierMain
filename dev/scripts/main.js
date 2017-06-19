a0ce81c3c9e288ae998959d1c0f28b939c1cdd68
Vincent Chevalier <info@vincentpchevalier.com>
2017-06-19 13:02:37 -0400

    reordered gallery and worked on styling for cross browser responsive

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,20 +1,20 @@
-// const mySite = {};
+const mySite = {};
 
-// mySite.init = function() {
-// 	mySite.events();
-// };
+mySite.init = function() {
+	mySite.events();
+};
 
-// mySite.events = () => {
-// 	//listens for click to open hamburger menu 
-// 	$('.hamburger').on('click', function(){
-// 		//toggle the overlay class on menu
-// 		$('.menu-bar').toggleClass('overlay');
+mySite.events = () => {
+	//listens for click to open hamburger menu 
+	$('.hamburger').on('click', function(){
+		//toggle the overlay class on menu
+		$('.menu-bar').toggleClass('overlay');
 
-// 		//toggle the show class on the ul inside of menu
-// 		$('.menu-bar ul').toggleClass('show');
-// 	});
-// }
+		//toggle the show class on the ul inside of menu
+		$('.menu-bar ul').toggleClass('show');
+	});
+}
 
-// $(function() {
-// 	mySite.init();
-// });
+$(function() {
+	mySite.init();
+});
