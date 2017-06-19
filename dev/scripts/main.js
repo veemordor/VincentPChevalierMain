daa56ebc2ff6107e49a9499e14af26b71d07f0a1
Vincent Chevalier <info@vincentpchevalier.com>
2017-06-19 00:50:52 -0400

    final responsive for Chrome - attempt at hamburger menu

---
+++ dev/scripts/main.js
@@ -0,0 +1,20 @@
+// const mySite = {};
+
+// mySite.init = function() {
+// 	mySite.events();
+// };
+
+// mySite.events = () => {
+// 	//listens for click to open hamburger menu 
+// 	$('.hamburger').on('click', function(){
+// 		//toggle the overlay class on menu
+// 		$('.menu-bar').toggleClass('overlay');
+
+// 		//toggle the show class on the ul inside of menu
+// 		$('.menu-bar ul').toggleClass('show');
+// 	});
+// }
+
+// $(function() {
+// 	mySite.init();
+// });
